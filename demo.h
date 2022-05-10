332d5bc777414813dec73b1da9dc6fd09bf7b86e
Pierre Dureau <dureaupierre53@gmail.com>
2022-05-10 09:37:52 +0200

    Fix betty

--- demo.h
+++ demo.h
@@ -5,6 +5,12 @@
 #include <stdio.h>
 #include <math.h>
 
+/**
+ * struct SDL_Instance - Instance of the window
+ *
+ * @window: Window
+ * @renderer: Renderer
+ */
 typedef struct SDL_Instance
 {
 	SDL_Window *window;
@@ -16,7 +22,8 @@ void InitialiseGrid(SDL_Point grid[8][8]);
 
 void IsometricGrid(SDL_Point grid[8][8], int altitudes[8][8]);
 void AltitudesGrid(int grid[8][8], char **argv);
-void RotateGrid(SDL_Point grid[8][8], SDL_Point rotgrid[8][8], int altitudes[8][8], float a, SDL_Instance instance);
+void RotateGrid(SDL_Point grid[8][8], SDL_Point rotgrid[8][8],
+	int altitudes[8][8], float a, SDL_Instance instance);
 void CopyGrid(SDL_Point grid[8][8], SDL_Point rotgrid[8][8]);
 void DrawGrid(SDL_Instance instance, SDL_Point grid[8][8]);
 
